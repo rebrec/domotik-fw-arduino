311126abc310c8d66ab72eb3ac02f5884de959b2
Francois Laplaud <el.francesco@gmail.com>
2014-12-12 20:47:37 +0100

    compile but doesn't send anything

--- CmdMgr.cpp
+++ CmdMgr.cpp
@@ -19,14 +19,14 @@ void CmdMgr::resetCmd(){
 void CmdMgr::processSerialData() {
 	char car;
 	String strCmd = "";
-	Serial.println("Attente Commande (ON/OFF)");
+	Serial.println("SALUT Attente Commande (ON/OFF)");
 	while(1) {
 		while(Serial.available() <= 0);
 		car = (char)Serial.read(); // lit un car 
 		if (car == CR){
 			this->validCMD(strCmd);
 			strCmd = "";
-			Serial.println("Attente Commande (ON/OFF)");
+			Serial.println("SALUT Attente Commande (ON/OFF)");
 			break;
 		} else {
 			strCmd += car;
@@ -36,13 +36,15 @@ void CmdMgr::processSerialData() {
 
 void CmdMgr::validCMD(String strCmd)
 {
-	  if (strCmd == "ON"){
-		  Serial.println("Commande : ON");
+	  if (strCmd == "ON"){ 
+		  Serial.println("Commandessss : ON");
+		  Serial.println(genCastoTrame(3,1,true));
 		  this->rcsw.send(genCastoTrame(3,1,true));// envoie ON sur le Chanel C (3) prise 2
 		  this->rcsw.send(genCastoTrame(3,2,true));// envoie ON sur le Chanel C (3) prise 2
 		  this->rcsw.send(genCastoTrame(3,3,true));// envoie ON sur le Chanel C (3) prise 2
 	  } else if (strCmd == "OFF"){
 		  Serial.println("Commande : OFF");
+		  Serial.println(genCastoTrame(3,1,false));
 		  this->rcsw.send(genCastoTrame(3,1,false));// envoie OFF sur le Chanel C (3) prise 2
 		  this->rcsw.send(genCastoTrame(3,2,false));// envoie OFF sur le Chanel C (3) prise 2
 		  this->rcsw.send(genCastoTrame(3,3,false));// envoie OFF sur le Chanel C (3) prise 2
